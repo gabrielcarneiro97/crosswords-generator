87c534cc7e74ab639a69db58156378ebb39d3783
Gabriel Carneiro <gabriel.carneiro97@live.com>
2020-05-27 17:07:49 -0300

    arrumando os role todo

--- elixir/lib/cwgen.ex
+++ elixir/lib/cwgen.ex
@@ -1,30 +1,41 @@
 defmodule Palavra do
-  defstruct charlist: '', pai: nil, thisId: nil, paiId: nil
+  defstruct charlist: '', pai: nil, cruzamento: {this: -1, pai: -1}, letras_cruzadas: []
 end
 
-
 defmodule Cwgen do
+  use Memoize
 
-  def is_palavra palavra do
-    case palavra do
-      %Palavra{} -> true
-      _ -> false
+  defmemo ler_lista_arquivo do
+    case File.read("../lista-palavras.txt") do
+      {:ok, body} -> String.split(body, "\r\n")
+      _ -> []
     end
   end
 
-  def novaPalavra string do
+  defp pegar_palavras lista, qnt, listaArquivo do
+    [head | tail] = Enum.shuffle(listaArquivo)
+    case qnt do
+      0 -> lista
+      _ -> lista ++ [head] |> pegar_palavras(qnt - 1, tail)
+    end
+  end
+
+  defp pegar_palavras lista, qnt do
+    listaArquivo = ler_lista_arquivo()
+    pegar_palavras(lista, qnt, listaArquivo)
+  end
+
+  def pegar_palavras(qnt), do: pegar_palavras [], qnt
+
+  def nova_palavra string do
     %Palavra{charlist: to_charlist(string)}
   end
 
-  def novaPalavra(string, palavraPai) when is_struct(palavraPai) do
-    %Palavra{charlist: to_charlist(string), pai: palavraPai}
+  def nova_palavra(string, palavra_pai) when is_struct(palavra_pai) do
+    %Palavra{charlist: to_charlist(string), pai: palavra_pai}
   end
 
-  def cruzarPalavras(palavra) when is_struct(palavra) do
-    case palavra do
-      %{pai: nil} -> "ué"
-      %{pai: %Palavra{}} -> "é palavra"
-      _ -> "resto"
-    end
+  def cruzar_palavras(palavra_pai, palavra_filha) do
+
   end
 end
